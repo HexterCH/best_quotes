b6b95ea240f395805126b36304bd23b22d747f8f
Hexter Weng <hexter.ch@gmail.com>
2016-07-05 02:12:34 +0800

    add quotes index page and add more json test case

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,6 +3,16 @@ class QuotesController < Rulers::Controller
     render :a_quote, noun: :winking
   end
 
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
+  def quote_1
+    quote_1 = FileModel.find(1)
+    render :quote, obj: quote_1
+  end
+
   def test_page
     "Test the rerun gem work or not ?"
   end
