01daec65cb6e3ba105f09093978b054f3aa87591
Hexter Weng <hexter.ch@gmail.com>
2016-07-09 01:12:45 +0800

    use render response in show page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -13,6 +13,12 @@ class QuotesController < Rulers::Controller
     render :quote, obj: quote_1
   end
 
+  def show
+    quote = FileModel.find(params["id"])
+    ua = request.user_agent
+    render_response :quote, obj: quote, ua: ua
+  end
+
   def new_quote
     attrs = {
       "submitter" => "web user",
